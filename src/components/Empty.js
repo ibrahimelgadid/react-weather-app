e0b0fb5e42cd0f45ce1f0cacfc357e51f51be856
ibrahimelgadid <38108517+ibrahimelgadid@users.noreply.github.com>
2023-03-26 11:53:44 +0200

    alert edit

--- src/components/Empty.js
+++ src/components/Empty.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Empty = () => {
   return (
-    <div className="text-white bg-black bg-opacity-50 text-3xl font-semibold p-10">
+    <div className="text-white bg-black bg-opacity-50 text-3xl font-semibold p-10 w-80 m-auto border">
       Give permission for your location or type city name and press Select Citty
     </div>
   );
